1cd8a0c5c26dfe1cd5068d1527f734610ac371df
gaofen13 <gaofen13@163.com>
2022-10-17 11:12:04 +0800

    新增Collapse组件

---
+++ DaisyBlazor/Components/Collapse/DaisyCollapse.razor.cs
@@ -0,0 +1,42 @@
+﻿using DaisyBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaisyBlazor
+{
+    public partial class DaisyCollapse
+    {
+        private string Classname =>
+            new ClassBuilder("collapse collapse-arrow")
+            .AddClass("collapse-open", Collapsed)
+            .Build();
+
+        [Parameter]
+        public bool Collapsed { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> CollapsedChanged { get; set; }
+
+        [Parameter]
+        public string? Title { get; set; }
+
+        [Parameter]
+        public RenderFragment? TitleContent { get; set; }
+
+        [Parameter]
+        public string? Message { get; set; }
+
+        [Parameter]
+        public RenderFragment? MessageContent { get; set; }
+
+        private async Task OnClickTitleAsync()
+        {
+            Collapsed = !Collapsed;
+            await CollapsedChanged.InvokeAsync(Collapsed);
+        }
+    }
+}
