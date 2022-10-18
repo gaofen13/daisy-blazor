9b83f2cb388215444d77a787eb686e0e92cfc1b7
gaofen13 <gaofen13@163.com>
2022-10-18 17:22:26 +0800

    新增menu组件

---
+++ DaisyBlazor/Components/Menu/DaisyMenu.razor.cs
@@ -0,0 +1,36 @@
+﻿using DaisyBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaisyBlazor
+{
+    public partial class DaisyMenu
+    {
+        private string Classname =>
+            new ClassBuilder("menu")
+            .AddClass("menu-horizontal", Horizontal)
+            .AddClass("menu-compact", Compact)
+            .AddClass("rounded-box", Rounded)
+            .AddClass(Class)
+            .Build();
+
+        [Parameter]
+        public bool Horizontal { get; set; }
+
+        [Parameter]
+        public bool Rounded { get; set; } = true;
+
+        [Parameter]
+        public bool Bordered { get; set; } = true;
+
+        [Parameter]
+        public bool Compact { get; set; }
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
---
+++ DaisyBlazor/Components/Menu/DaisyMenuItem.razor.cs
@@ -0,0 +1,49 @@
+﻿using DaisyBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaisyBlazor
+{
+    public partial class DaisyMenuItem
+    {
+        private string Classname =>
+            new ClassBuilder()
+            .AddClass("disabled", Disabled)
+            .AddClass("bordered", Menu?.Bordered == true)
+            .AddClass(Class)
+            .Build();
+
+        private Dictionary<string, object>? NavLinkAttributes => Disabled || string.IsNullOrWhiteSpace(Href) ? null : new Dictionary<string, object> { { "href", Href } };
+
+        [CascadingParameter]
+        private DaisyMenu? Menu { get; set; }
+
+        [Parameter]
+        public string? Href { get; set; }
+
+        [Parameter]
+        public string? Text { get; set; }
+
+        [Parameter]
+        public NavLinkMatch Match { get; set; } = NavLinkMatch.Prefix;
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+        private void OnHandlerClick()
+        {
+            if (string.IsNullOrWhiteSpace(Href))
+            {
+                return;
+            }
+        }
+    }
+}
