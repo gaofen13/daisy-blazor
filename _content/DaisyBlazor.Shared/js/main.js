63ed542ced94457de16e08b1c7a4f1cc64870e96
gaofen13 <gaofen13@163.com>
2022-10-22 14:56:59 +0000

    Deploying to gh-pages from  @ c246810dde465b14dec8be8fe1f391c592c7c181 🚀

---
+++ _content/DaisyBlazor.Shared/js/main.js
@@ -0,0 +1,19 @@
+﻿let windowEventListeners = {};
+
+function AddWindowWidthListener(objReference) {
+    let eventListener = () => UpdateWindowWidth(objReference);
+    window.addEventListener("resize", eventListener);
+    windowEventListeners[objReference] = eventListener;
+}
+
+function RemoveWindowWidthListener(objReference) {
+    window.removeEventListener("resize", windowEventListeners[objReference]);
+}
+
+function UpdateWindowWidth(objReference) {
+    objReference.invokeMethodAsync("UpdateWindowWidth", window.innerWidth);
+}
+
+function GetWindowWidth() {
+    return window.innerWidth;
+}
