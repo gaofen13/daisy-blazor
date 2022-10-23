2acea59c6f039923380aaffe1a3b3b2cc72c570b
gaofen13 <gaofen13@163.com>
2022-10-23 15:53:50 +0800

    修改card架构

--- DaisyBlazor/Components/Card/DaisyCard.razor.cs
+++ DaisyBlazor/Components/Card/DaisyCard.razor.cs
@@ -11,11 +11,27 @@ namespace DaisyBlazor
             .AddClass("card-bg-base", !Neutra)
             .AddClass("card-bg-neutral", Neutra)
             .AddClass("card-glass", Glass && !Neutra)
+            .AddClass($"card-img-{ImgPostion.ToString().ToLower()}")
             .AddClass(Class)
             .Build();
 
         [Parameter]
-        public RenderFragment? ChildContent { get; set; }
+        public string? Title { get; set; }
+
+        [Parameter]
+        public RenderFragment? TitleTemplate { get; set; }
+
+        [Parameter]
+        public RenderFragment? ImgTemplate { get; set; }
+
+        [Parameter]
+        public Position ImgPostion { get; set; }
+
+        [Parameter]
+        public RenderFragment? ActionTemplate { get; set; }
+
+        [Parameter]
+        public RenderFragment? ContentTemplate { get; set; }
 
         [Parameter]
         public bool Compact { get; set; }
@@ -25,5 +41,8 @@ namespace DaisyBlazor
 
         [Parameter]
         public bool Glass { get; set; }
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
     }
 }
