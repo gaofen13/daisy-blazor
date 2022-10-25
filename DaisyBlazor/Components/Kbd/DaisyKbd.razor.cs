868dc54cb7030d8b661d32add3e66d7c45d7f0e8
gaofen13 <gaofen13@163.com>
2022-10-25 08:54:11 +0800

    新增kbd组件

---
+++ DaisyBlazor/Components/Kbd/DaisyKbd.razor.cs
@@ -0,0 +1,28 @@
+﻿using DaisyBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaisyBlazor
+{
+    public partial class DaisyKbd
+    {
+        private string KdbClass =>
+            new ClassBuilder("kbd")
+            .AddClass(Class)
+            .Build();
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        [Parameter]
+        public bool StopPropagation { get; set; }
+    }
+}
