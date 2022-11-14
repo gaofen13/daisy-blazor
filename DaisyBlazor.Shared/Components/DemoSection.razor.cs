b3cd51573ce7bfb564a40c9cc2df4d7a8e0214ad
gaofen13 <gaofen13@163.com>
2022-11-14 17:06:24 +0800

    改善tab显示 测试增加源码

---
+++ DaisyBlazor.Shared/Components/DemoSection.razor.cs
@@ -0,0 +1,46 @@
+﻿using DaisyBlazor.Generators;
+using Microsoft.AspNetCore.Components;
+using System.Reflection.Metadata;
+
+namespace DaisyBlazor.Shared.Components
+{
+    public partial class DemoSection
+    {
+        [Parameter, EditorRequired]
+        public string Title { get; set; } = string.Empty;
+
+        [Parameter]
+        public RenderFragment? Description { get; set; }
+
+        [Parameter, EditorRequired]
+        public string ExampleFile { get; set; } = string.Empty;
+
+        private string? CodeContents { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                if (!string.IsNullOrEmpty(ExampleFile))
+                {
+                    SetCodeContents();
+                }
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        protected void SetCodeContents()
+        {
+            try
+            {
+                CodeContents = DemoSnippets.GetRazor($"{ExampleFile}");
+                StateHasChanged();
+            }
+            catch
+            {
+                //Do Nothing
+            }
+        }
+    }
+}
