25b0220b04417434c76da3b8d4f860f4fa1800a6
gaofen13 <gaofen13@163.com>
2022-11-15 09:41:41 +0800

    合并code组件

--- DaisyBlazor/Components/Code/DaisyMockupCode.razor.cs
+++ DaisyBlazor/Components/Code/DaisyMockupCode.razor.cs
@@ -1,12 +1,22 @@
 ﻿using DaisyBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
 
 namespace DaisyBlazor
 {
     public partial class DaisyMockupCode
     {
-        private string MockupClass =>
-            new ClassBuilder("mockup-code")
-            .AddClass(Class)
+        private string PrefixClass =>
+            new ClassBuilder()
+            .AddClass("code-data-prefix", !string.IsNullOrWhiteSpace(DataPrefix))
             .Build();
+
+        [Parameter]
+        public string? Id { get; set; }
+
+        [Parameter]
+        public string? DataPrefix { get; set; }
+
+        [Parameter]
+        public string? CodeText { get; set; }
     }
 }
