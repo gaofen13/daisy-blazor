64a9e117dec77531d2c8303b2d9f60e0238b238e
gaofen13 <gaofen13@163.com>
2022-11-25 20:59:10 +0800

    分离theme，demo增加theme选择

--- DaisyBlazor.Shared/Shared/MainLayout.razor.cs
+++ DaisyBlazor.Shared/Shared/MainLayout.razor.cs
@@ -1,7 +1,13 @@
-﻿namespace DaisyBlazor.Shared.Shared
+﻿using DaisyBlazor.Shared.Data;
+
+namespace DaisyBlazor.Shared.Shared
 {
     public partial class MainLayout
     {
         private bool showSidebar = true;
+
+        private string? theme;
+
+        private string[] daisyTheme = { "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter" };
     }
 }
